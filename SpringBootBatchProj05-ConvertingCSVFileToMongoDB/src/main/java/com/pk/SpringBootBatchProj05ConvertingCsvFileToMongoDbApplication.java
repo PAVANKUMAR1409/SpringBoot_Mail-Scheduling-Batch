11a992b752b77ed481395a2973a347f296abe785
# E.-PAVANKUMAR <pavanelagandula@gmail.com>
2024-06-29 07:07:51 +0530

    Batch-5

---
+++ SpringBootBatchProj05-ConvertingCSVFileToMongoDB/src/main/java/com/pk/SpringBootBatchProj05ConvertingCsvFileToMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchProj05ConvertingCsvFileToMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchProj05ConvertingCsvFileToMongoDbApplication.class, args);
+	}
+
+}
